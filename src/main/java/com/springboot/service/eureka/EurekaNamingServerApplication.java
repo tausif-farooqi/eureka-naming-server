15fc9bdb7e1ef9c0a785de90a555fd586aae37f5
tausif-farooqi <tausif.farooqi@gmail.com>
2019-03-24 18:19:37 -0700

    Downgraded spring boot version to 2.1.3

--- src/main/java/com/springboot/service/eureka/EurekaNamingServerApplication.java
+++ src/main/java/com/springboot/service/eureka/EurekaNamingServerApplication.java
@@ -1,8 +1,10 @@
-package com.springboot.service.eurekanamingserver;
+package com.springboot.service.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaNamingServerApplication {
 
